9890b37c359293180893ca4954354bb3b68da71f
huytht <huytht2001@gmail.com>
2021-03-26 16:46:15 +0700

    Add Admin
    Update Backend

---
+++ admin/blocks/foot.php
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="public/templates/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="public/templates/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="public/templates/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="public/templates/dist/js/demo.js"></script>
---
+++ admin/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<?php include("blocks/head.php") ?>
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+	<div class="wrapper">
+		<!-- Navbar	-->
+		<?php include("blocks/navbar.php")?>
+		<!-- Navbar	-->
+		
+		<!-- Sidebar -->
+		<?php include("blocks/sidebar.php")?>
+		<!-- Sidebar -->
+		
+		<!-- Content -->
+		<?php include("blocks/content.php")?>
+		<!-- Content -->
+		
+		<!-- Footer -->
+		<?php include("blocks/footer.php")?>
+		<!-- Footer -->
+		
+		<!-- Control Sidebar -->
+		<?php include("blocks/control_sidebar.php")?>
+		<!-- Control Sidebar -->
+	</div>
+</body>
+</html>
