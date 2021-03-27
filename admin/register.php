91741c7dc8818adb67611f7f93116d5d56c0c2d7
huytht <huytht2001@gmail.com>
2021-03-27 09:06:44 +0700

    Update Admin Register Form
    Update Admin Register Form

---
+++ admin/register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include("blocks/head_register.php");
+    ?>
+</head>
+
+<body class="hold-transition register-page">
+    <div class="register-box">
+        <?php
+        include("blocks/register_box.php");
+        ?>
+    </div>
+    <?php
+    include("blocks/foot_register.php");
+    ?>
+</body>
+
+</html>
